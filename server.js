8b24ebd7451daa3c9c4fa2cea7c095abab3a4fe6
fahmysharaf <fahmysharf@gmail.com>
2020-12-05 17:40:29 +0200

    Initial Author Routes

--- server.js
+++ server.js
@@ -4,15 +4,20 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
