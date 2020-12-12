1e056ca358cabeb2eb7e5d486537d7bc11621630
fahmysharaf <fahmysharf@gmail.com>
2020-12-12 05:50:59 +0200

    Finished

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
@@ -12,23 +13,21 @@ const authorRouter = require("./routes/authors");
 const bookRouter = require("./routes/books");
 
 app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
+app.use(methodOverride("_method"));
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
-app.use(methodOverride("_method"));
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
+
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
-
-const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected To mongoose"));
 
 app.listen(process.env.PORT || 3000);
